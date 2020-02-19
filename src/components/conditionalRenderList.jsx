0ff6113b3e216400e8dbc17c2ba6f82b6f7c6d0b
Gabriel Lopez <lopezg3000@gmail.com>
2020-02-18 21:00:20 -0600

    PassingEventArguments

---
+++ src/components/conditionalRenderList.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class DynamicList extends Component {
+    state = {
+        count: 0,
+        tags: []
+    };
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags</p>;
+
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                {this.state.tags.length === 0 && "Need to Create Tags!"}
+                {this.renderTags()}
+            </React.Fragment>
+        );
+    };
+
+};
+
+
+export default DynamicList;
---
+++ src/components/counter.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        count: 0,
+    };
+
+    handleIncrement = product => {
+        this.setState({ count: this.state.count + 1 });
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={() => this.handleIncrement(1)} className="btn btn-secondary btn-sm">Increment</button>
+            </React.Fragment>
+        );
+    };
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? "zero" : count;
+    };
+};
+
+
+export default Counter;
