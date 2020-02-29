b3f7fff61eb946e66cfca2c47d063119ce8487cc
Gabriel Lopez <lopezg3000@gmail.com>
2020-02-29 12:31:17 -0600

    decrementDisabled

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -20,7 +20,7 @@ class Counter extends Component {
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">+</button>
-                <button onClick={() => this.props.onDecrement(this.props.counter)} className="btn btn-secondary btn-sm m-2">-</button>
+                <button onClick={() => this.props.onDecrement(this.props.counter)} className="btn btn-secondary btn-sm m-2" disabled={this.buttonDisabled()}>-</button>
                 <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2" >x</button>
 
             </div>
@@ -37,6 +37,11 @@ class Counter extends Component {
         const { value } = this.props.counter;
         return value === 0 ? "zero" : value;
     };
+
+    buttonDisabled() {
+        const { value } = this.props.counter;
+        return value > 0 ? false : true;
+    }
 };
 
 
